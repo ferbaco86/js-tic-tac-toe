03842c80663a8e2169e8183a886ca6dc50dae3c8
Fernando Bahamondes <ferbaco@gmail.com>
2020-09-02 16:18:02 -0400

    Add players symbol parameters to domListener function

--- index.js
+++ index.js
@@ -1,27 +1,6 @@
 const gameBoard = {
   board: ['', '', '', '', '', '', '', '', ''],
 };
-let counterPlays = 0;
-const domListener = (event) => {
-  if (event.target.nodeName === "DIV") {
-    const target = event.target;
-    let id = target.getAttribute('id');
-    if (counterPlays % 2 === 0 && gameBoard.board[id] === '') {
-      target.innerHTML = 'X';
-      gameBoard.board[id] = "X";
-      counterPlays += 1;
-    }
-    else if (gameBoard.board[id] === '') {
-      target.innerHTML = 'O';
-      gameBoard.board[id] = 'O';
-      counterPlays += 1;
-    }
-  }
-}
-
-const boardSection = document.getElementById('board');
-boardSection.addEventListener('click', domListener)
-
 
 const player = (name, symbol) => {
   const getName = () => name;
@@ -29,4 +8,25 @@ const player = (name, symbol) => {
   return { getName, getSymbol };
 };
 
+const player1 = player('John', 'X');
+const player2 = player('Jude', 'O');
 
+let counterPlays = 0;
+const domListener = (p1Symbol, p2Symbol) => {
+  if (event.target.nodeName === 'DIV') {
+    const { target } = event;
+    const id = target.getAttribute('id');
+    if (counterPlays % 2 === 0 && gameBoard.board[id] === '') {
+      target.innerHTML = p1Symbol;
+      gameBoard.board[id] = p1Symbol;
+      counterPlays += 1;
+    } else if (gameBoard.board[id] === '') {
+      target.innerHTML = p2Symbol;
+      gameBoard.board[id] = p2Symbol;
+      counterPlays += 1;
+    }
+  }
+};
+
+const boardSection = document.getElementById('board');
+boardSection.addEventListener('click', domListener.bind(event, player1.getSymbol(), player2.getSymbol()));
