e71f0f23f740f3d86602c4badd04196c0de7313f
Fernando Bahamondes <ferbaco@gmail.com>
2020-09-02 12:43:03 -0400

    Add gameboard object, basic html structure and player factory

---
+++ index.js
@@ -0,0 +1,13 @@
+const gameBoard = {
+  board: ['X', 'X', 'O', 'O', 'X', 'X', 'O', 'X', 'O'],
+};
+
+const boardSection = document.getElementById('board');
+
+const player = (name, symbol) => {
+  const getName = () => name;
+  const getSymbol = () => symbol;
+  return { getName, getSymbol };
+};
+
+
